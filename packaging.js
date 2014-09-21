b0902a308111936b8903f3cc42031c28c8f8b5ea
Sarkis Peha <sarkispeha@gmail.com>
2014-09-21 15:28:49 -0600

    added googlemaps to the packaging listing page with the intent of showing where item is once I learn how

--- packaging.js
+++ packaging.js
@@ -7,14 +7,22 @@ $('.tbody-orig tr').hover(function() {
 , function(){
 	$(this).removeClass('tbody-hover').addClass('tbody-orig');
 	}
-	);
+);
 
+$('.tbody-orig tbody').on('click', 'td', function() {
 
-
+} )
 // click on row to go to details
 
-
-
-
-
 });
+
+function initialize() {
+		var mapCanvas = document.getElementById('map-canvas');
+		var mapOptions = {
+      	center: new google.maps.LatLng(44.5403, -78.5463),
+     	zoom: 8,
+      	mapTypeId: google.maps.MapTypeId.ROADMAP
+    	}
+		var map = new google.maps.Map(mapCanvas, mapOptions);
+	}
+	google.maps.event.addDomListener(window, 'load', initialize);
