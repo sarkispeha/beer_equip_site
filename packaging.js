2c74a9f43ee06e88ced92de960b50486e02594a4
Sarkis Peha <sarkispeha@gmail.com>
2014-09-27 12:27:45 -0600

    googlemap location shows on button click, not specific for location however

--- packaging.js
+++ packaging.js
@@ -1,5 +1,7 @@
 $(document).on('ready',function(){
 
+
+
 // make hovered row highlighted
 $('.tbody-orig tr').hover(function() {
 	$(this).removeClass('tbody-orig').addClass('tbody-hover');
@@ -9,61 +11,25 @@ $('.tbody-orig tr').hover(function() {
 	}
 );
 
-$('tbody').on('click', 'td', function() {
-	console.log('works');
-} )
 
 // click on row to go to details
 
-// var geocoder;
-// var map;
-
-// function initialize() {
-
-// 		geocoder = new google.maps.Geocoder();
-// 		var mapCanvas = document.getElementById('map-canvas');
-// 		var mapOptions = {
-//       	center: new google.maps.LatLng(40.03, -105.25),
-//      	zoom: 12,
-//       	mapTypeId: google.maps.MapTypeId.ROADMAP
-//     	}
-// 		map = new google.maps.Map(mapCanvas, mapOptions);
-// 	}
-// google.maps.event.addDomListener(window, 'load', initialize);
-
-
-
-// function codeAddress() {
-//     var address = $("#addy").text();
-//     geocoder.geocode( { 'address': address}, function(results, status) {
-//       if (status == google.maps.GeocoderStatus.OK) {
-//         map.setCenter(results[0].geometry.location);
-//         var marker = new google.maps.Marker({
-//             map: map,
-//             position: results[0].geometry.location
-//         });
-//       } else {
-//         alert("Geocode was not successful for the following reason: " + status);
-//       }
-//     });
-//   }
 
 var geocoder;
 var map;
 
  var initialize = function () {
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var latlng = new google.maps.LatLng(40.05, -113.64);
     var mapOptions = {
       zoom: 8,
       center: latlng
     }
     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
   }
- 
 
- var codeAddress = function() {
+var codeAddress = function() {
     geocoder = new google.maps.Geocoder();
-    var address = $('#addy').val(); //$("#addy").text();
+    var address = $('#addy').text(); //$("#addy").text();
     geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         map.setCenter(results[0].geometry.location);
@@ -77,7 +43,13 @@ var map;
     });
   }
 
-google.maps.event.addDomListener(window, 'load', initialize);
-google.maps.event.addDomListener(window, 'load', codeAddress);
+// google.maps.event.addDomListener(window, 'load', initialize);
+// google.maps.event.addDomListener(window, 'load', codeAddress);
+
+$('td').on('click', 'button', function() {
+	console.log('works');
+	initialize();
+	codeAddress();
+} )
 
 });
