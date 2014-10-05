4132cba3fe835dd40535f6b926667fe7c2be3ec7
Sarkis Peha <sarkispeha@gmail.com>
2014-10-05 17:14:02 -0600

    refactored jquery hover with mouseover and mouseout handlers

--- packaging.js
+++ packaging.js
@@ -3,13 +3,14 @@ $(document).on('ready',function(){
 
 
 // make hovered row highlighted
-// $('.tbody-orig tr').hover(function() {
-// 	$(this).removeClass('tbody-orig').addClass('tbody-hover');
-// 	}
-// , function(){
-// 	$(this).removeClass('tbody-hover').addClass('tbody-orig');
-// 	}
-// );
+$(document).on('mouseover','.tbody-orig tr', function() {
+	$(this).removeClass('tbody-orig').addClass('tbody-hover');
+	});
+
+
+$(document).on('mouseout', '.tbody-orig tr',function(){
+	$(this).removeClass('tbody-hover').addClass('tbody-orig');
+	});
 
 
 // click on row to go to details
