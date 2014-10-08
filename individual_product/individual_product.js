f192cebd9613d4ed2947a148780a4e9a9488de8c
Sarkis Peha <sarkispeha@gmail.com>
2014-10-08 11:31:33 -0600

    faked shipping price calculator

--- individual_product/individual_product.js
+++ individual_product/individual_product.js
@@ -1,5 +1,13 @@
 $(document).on('ready', function() {
-  
+
+//verification for shipping button  
+$(document).on('click', '.shippingBtn', function() {
+	// if(#streetInput === '') {
+	// 	alert()
+	// }
+	$('.priceCalculated').show();
+	})
+
 
 $(document).on('click', '.seller-contact', function() {
 	$(this).remove().addClass('seller-hidden');
