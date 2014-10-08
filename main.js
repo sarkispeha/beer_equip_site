c2ebd833ec6545c3bc99edcd7d80032f0db922f1
Sarkis Peha <sarkispeha@gmail.com>
2014-10-08 15:49:28 -0600

    a bit of animation added to main page for products

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 $(document).on('ready',function(){
 
 
-$(document).on('mouseover','.tanks h3', function() {
+$(document).on('mouseover','.packaging h3', function() {
 	console.log('work');
-	
+	$('.packaging-hidden').find('p')
+		.show(400)
+		.animate({
+			left:'200px',
+			});
+	$('.packaging-hidden').find('img')
+		.show(400)
+		.animate({
+			right:'200px',
+			});
 })
+
 
 
 
